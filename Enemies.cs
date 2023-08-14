2341ac4c589375218ada1cadde648f88473d4ea7
hihowyoudoings <122053077+hihowyoudoings@users.noreply.github.com>
2023-08-14 14:25:07 -0500

    Update Enemies.cs

--- Enemies.cs
+++ Enemies.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemies : MonoBehaviour {
-    public static object[,] enemiesStats = {  { "testenemy1", 5, 5, 5 }, { "testenemy2", 25, 25, 25 } ,
-        { "testenemy3", 125, 125, 125 }  };
+    public static object[,] enemiesStats = {  { "Slime", 5, 5, 5 }, { "Rat", 4, 7, 3 } ,
+        { "Small Dog", 10, 4, 5 }, {"Cat",6,7,3}, {"Small Snake", 10 ,8,6},{"Child",9,7,8},{"Average Dog", 12,6,7},{"Swine",10,8,8},{"Large Dog",15,8,9},
+        {"Skeleton",10,12,10},{"Zombie",13,8,12}, {"Man"}};
 
     public static object[,] moves = { { "testmove",40, 0, "a test-tacular move" }, { "testmove2",80, 1, "a test-tacular move squared" } };
 
